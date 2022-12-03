4bc7f99ff34a3fcddc5ac64fc64b8dee5effb14a
Lauren Knight <lknight2@mail.umw.edu>
2022-12-03 14:48:05 -0500

    refactor

--- day3.py
+++ day3.py
@@ -1,21 +1,21 @@
-with open('3.txt') as f:
-    input3 = [line.strip() for line in f]
-
 def priority(c):
     ascii = ord(c)
     if ascii <= ord('Z'):
         return ascii - ord('A') + 27
     return ascii - ord('a') + 1
-    
+
+with open('3.txt') as f:
+    input3 = [line.strip() for line in f]
+  
 p1sum = 0
 p2sum = 0
 i = 1
 for line in input3:
     half = len(line) // 2
-    first_set = { *line[:half] }
-    second_set = { *line[half:] }
-    in_both, = first_set.intersection(second_set)
-    p1sum += priority(in_both)
+    first_half_set = { *line[:half] }
+    second_half_set = { *line[half:] }
+    item_in_both, = first_half_set.intersection(second_half_set)
+    p1sum += priority(item_in_both)
     
     if i == 1:
         badge_set = { *line }
